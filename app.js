b433dfa5169012aea5dd4292353fe6553283102f
[JoseFR2001] <[jose.rafael.mendoza.2001@gmail.com]>
2025-05-12 01:01:10 -0300

    Trabajo Finalizadp

---
+++ app.js
@@ -0,0 +1,36 @@
+//Declaramos las variables//
+let dolar, pesos, monto, montoDolar,opcion;
+dolar=1300;
+const history=[];
+
+//Creamos la función//
+function convertCurrency(pesos){
+    return pesos/dolar
+}
+
+//Creamos el bucle//
+do{
+    opcion = prompt('Elija una opción \n1) Divisa (ARS -> USD) \n2) Salir');
+    switch(opcion){
+        case "1":
+            monto=parseFloat(prompt('Monto en pesos'));
+            montoDolar= convertCurrency(monto).toFixed(2);
+            alert(montoDolar + ' USD');
+
+            //Pusheamos los datos a la array//
+            history.push({
+                tipo: 'divisa',
+                entrada: monto+' ARS',
+                salida: montoDolar+' USD',
+                fecha: new Date().toLocaleString()
+            });
+            console.log(history)
+            break
+        case "2":
+            alert('Salió');
+            break;
+        default:
+            alert('No ingreso un valor valido');
+    }
+}while (opcion!=='2') 
+
